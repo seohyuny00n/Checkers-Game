18996b59449cfb47ba070260979689247e163f34
seohyuny00n <124318783+seohyuny00n@users.noreply.github.com>
2023-08-03 22:23:27 +0000

    completed first draft of menu GUI view

--- menu_gui_view.py
+++ menu_gui_view.py
@@ -49,7 +49,7 @@ QWidget {
             "Enter_YN_Confirm": QLineEdit(),
             "Next": QPushButton("Next")
         }
-        
+
         # adding the widgets to layout
         self.form_layout.addRow(self.widgets["GameName"])
         self.form_layout.addRow(self.widgets["Instructions1"])
@@ -68,17 +68,80 @@ QWidget {
 
         self.menu_screen_model = MenuScreen()
 
-    def p1_name_entered(self):
-        """When the user inputs characters (hopefully) within QLineEdit."""
-        self.player_one_name = ""
-        if self.player_one_name == "":
-        # if the user entered a valid response, ask player 2
-    
-    def p2_name_entered(self):
+    # add user input for y code here (from model)
+    def user_input_entered(self):
+        """When the user is asked to input Y to proceed (from QLineEdit)."""
+        user_input_proceed  = self.widgets["EnterY"].text().upper()
+        if user_input_proceed == "Y":
+            # when the user successfully inputs a Y (proceed = pos), proceed to ask for player information, next function
+            self.ask_player_information()
+        else:
+            # if the user inputs a character that is anything outside of Y.
+            print("Please enter 'Y' to begin.")
+
+    # ask player information (from model)
+    def ask_player_information(self):
+        """Users will be asked for player names to be referred by. Additional info also given."""
+        # instructions?
+        print("Please decide who will be Player 1 (Black pieces) and Player 2 (White pieces).")
+        print("Player 1 (Black) will have their turn first.")
+
+        # player 1 name request
+        self.player_one_name = input("Enter Player 1's name here:")
+
+        # player 2 name request
+        self.player_two_name = input("Enter Player 2's name here:")
+
+        # proceed to confirm names
+        self.name_confirmation()
+
+    # name confirmation (also pulled from model)
+    def name_confirmation(self):
+        """Users will be asked to confirm their inputted(?) names."""
+        print("Is this information correct? (Y/N)")
+        print("Player 1: " + self.player_one_name)
+        print("Player 2: " + self.player_two_name)
+
+        # hide widgets not needed currently. only show confirmation widget. only showing --> does that work?
+        self.widgets["Instructions1"].hide()
+        self.widgets["EnterY"].hide()
+        self.widgets["Enter_P1_Name"].hide()
+        self.widgets["Enter_P2_Name"].hide()
+        self.widgets["Enter_YN_Confirm"].show()
+        self.widgets["Next"].show()
 
     def confirm_names_entered(self):
-    
+        """When the user confirms the names that they have inputted (saying it's correct)."""
+        # source for getting response: ??? (NEED TO PUT IN)
+        user_response = self.widgets["Enter_YN_Confirm"].text().upper()
+
+        # if user responds positively (Y)
+        if user_response == "Y":
+            # the next button must allow the user to move on to the next window.
+            print("Next")
+        elif user_response == "N":
+            # direct (show) the user(s) widgets again asking for name + instructions.
+            self.widgets["Enter"].show()
+            self.widgets["Enter_P1_Name"].show()
+            self.widgets["Enter_P2_Name"].show()
+            self.widgets["Enter_YN_Confirm"].hide()
+            self.widgets["Next"].hide()
+        else:
+            # if the user responds with a character that is out of boundaries (/not y nor n).
+            print("Error. Must enter 'Y' or 'N'. Please re-enter response.")
+
+    def p1_name_entered(self):
+        """When the user inputs characters (hopefully) within QLineEdit."""
+        pass
+        # if the user entered a valid response, ask player 2
+
+    def p2_name_entered(self):
+        """When the user inputs characters (hopefully) for QLineEdit."""
+        pass
+
     def next_button_clicked(self):
+        """WIP, shows the next screen, which is either the rules or the main checker board screen."""
+        pass
 
 
 
@@ -88,19 +151,3 @@ menu = MenuScreenWindow()
 
 menu.show()
 app.exec()
-
-    def user_input_entered(self):
-        """When the user enters Y on QLineEdit."""
-        # source 1: https://stackoverflow.com/questions/66341365/how-do-i-get-the-text-of-an-qlineedit-into-a-method
-        # source 2: https://stackoverflow.com/questions/42288320/python-how-to-get-qlineedit-text
-        user_reponse = self.widgets["EnterY"].text().strip().upper()
-        if user_response == "Y":
-            # proceed to ask for player names & give additional info
-            self.ask_player_information()
-        else:
-            # if the player inputs a response that is not 'y'.
-            print("Please enter 'Y' to begin.")
-            self._user_response = str(input()).upper()
-            # once the user responds within bounds (y), the next prompt will be asked straight-away
-            if self._user_response == "Y":
-                self.ask_player_information()
